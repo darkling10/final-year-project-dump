2e80586eca4640ab2f32bd9019f6bf9319742b2e
Abbas Pathan <pathan.abbas10@gmail.com>
2023-08-16 11:49:41 +0530

    Download PDF service added

--- misc-scripts/pdf-processing/src/main/java/com/example/pdfprocessing/process/PdfProcess.java
+++ misc-scripts/pdf-processing/src/main/java/com/example/pdfprocessing/process/PdfProcess.java
@@ -1,24 +1,29 @@
 package com.example.pdfprocessing.process;
 
 import com.example.pdfprocessing.requestSchemas.PdfArrayJson;
+import com.example.pdfprocessing.service.PDFservices;
+import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.util.List;
 
 @RestController
 @RequestMapping("/process/pdf")
+@AllArgsConstructor
 public class PdfProcess {
+
+    private final PDFservices pdFservices;
+
     @GetMapping("/get")
     public ResponseEntity<Object> getCheck(){
         return ResponseEntity.status(200).body("Hello from proccess API");
     }
 
-    @GetMapping("/download/json")
-    public ResponseEntity<Object> downloadPDF(@RequestBody List<PdfArrayJson> pdfArrayJson){
+    @PostMapping("/download/json")
+    public ResponseEntity<Object> downloadPDF(@RequestBody List<PdfArrayJson> pdfArrayJson) throws IOException, InterruptedException {
+        pdFservices.downloadPDFService(pdfArrayJson);
         return ResponseEntity.status(200).body(pdfArrayJson);
     }
 }
---
+++ misc-scripts/pdf-processing/src/main/java/com/example/pdfprocessing/service/PDFservices.java
@@ -0,0 +1,63 @@
+package com.example.pdfprocessing.service;
+
+import com.example.pdfprocessing.GlobalVariables;
+import com.example.pdfprocessing.requestSchemas.PdfArrayJson;
+import org.springframework.stereotype.Service;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.List;
+
+@Service
+public class PDFservices {
+
+    private static HttpRequest getHttpRequest(String uri) {
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .GET()
+                .build();
+        return request;
+    }
+
+
+    private static HttpResponse<InputStream> getResponse(HttpRequest request) throws IOException, InterruptedException {
+        return HttpClient
+                .newHttpClient()
+                .send(request, HttpResponse.BodyHandlers.ofInputStream());
+    }
+
+    private static void downloadPDF(String pdfFile,String FileName) throws IOException, InterruptedException {
+        HttpRequest request = getHttpRequest(pdfFile);
+        HttpResponse<InputStream> response = getResponse(request);
+
+        System.out.println("status code: " + response.statusCode());
+        InputStream body = response.body();
+
+        WriteLocal(body, FileName);
+    }
+
+    private static void WriteLocal(InputStream body, String destinationFIle) throws IOException {
+        FileOutputStream fos = new FileOutputStream(destinationFIle);
+
+//        body.close();
+        fos.write(body.readAllBytes());
+        fos.close();
+    }
+
+    public void downloadPDFService(List<PdfArrayJson> pdfArrayJson) throws IOException, InterruptedException {
+        for (PdfArrayJson pdfArrayJson1: pdfArrayJson){
+            String pdfName = pdfArrayJson1.getPdfName();
+            String downloadURL = pdfArrayJson1.getDownloadURL();
+            String description = pdfArrayJson1.getDescription();
+            String filePath = GlobalVariables.filePathPDF+pdfName+".pdf";
+
+            downloadPDF(downloadURL,filePath);
+
+        }
+    }
+}
